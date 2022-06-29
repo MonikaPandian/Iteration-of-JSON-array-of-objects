b7bfeaa1db974b126e45ca0e30e0f9cc0fd3de00
MonikaPandian <87970544+MonikaPandian@users.noreply.github.com>
2022-06-29 16:59:20 +0530

    JSON resume

---
+++ jsonresume.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "Monika Pandian",
+      "email": "panmonikmm@gmail.com",
+      "phone":  8489703540,
+      "degree": "B.E",
+      "summary" :"Hard worker, quick learner, and ability to assume responsibility", 
+      "location": {
+        "address": "Dreamland Kilpallipattu",
+        "postalCode": 632311,
+        "city": "Vellore",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": 
+        {
+          "github":"https://github.com/MonikaPandian"
+        }
+      
+    },
+    
+    "education":[ 
+      { 
+        "school name": "Seventh-Day Adventist Higher Secondary School",
+        "college name": "C.Abdul Hakeem College of Engineering and Technology",
+        "department": "Computer Science",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+        "gpa": 7.56,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python, Javascript, C, C++, Java, HTML, CSS",
+        "project": "car Pooling  using Big Data"
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil, English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Art, Architecture, Painting, Sports",
+        "keywords" : "Shuttlecock, Chess, Carrom, Cricket, Painting"
+      }
+    ]
+  }
+  console.log(myResume);
+  
