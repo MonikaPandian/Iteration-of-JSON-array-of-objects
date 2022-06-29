ad5feb233f658ebcdf799e874ed88d27972527bd
MonikaPandian <87970544+MonikaPandian@users.noreply.github.com>
2022-06-29 16:58:25 +0530

    Iteration of JSON over all for loops

---
+++ iterationjson.js
@@ -0,0 +1,36 @@
+//Step 1:creating an XHR(XML-HTTP Request) object
+//XML-HTTP Request are a way to interact with the servers via API
+//API is restcountries
+var request=new XMLHttpRequest();
+//Step 2:create a request/open a request
+//open is an inbuilt method.open method consists of two parameters:HTTP method,API-URL
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+//Step 3:creating a bridge
+request.send();
+//Step 4:Status code for success:200
+//once the data successfully received from the server
+//the data travelling from the server
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+  
+   //Iterating through for loop
+for(var i=0;i<result.length;i++){
+  console.log(result[i]);
+}
+//Iterating through for-of loop
+for(let s of result){
+  console.log(s);
+}
+
+//Iterating through for-each loop
+result.forEach(element => console.log(element));
+
+//Iterating through for-in loop
+for (var key in result) {
+  console.log(result[key]);
+  
+}
+
+
+}
